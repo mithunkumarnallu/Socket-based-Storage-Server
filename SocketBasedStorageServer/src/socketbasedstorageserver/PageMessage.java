d6bb2f204d4222bf63adf2c6919b7587d21b554f
ashwinrpi <ashwinbahulkar@gmail.com>
2015-05-05 00:41:59 -0400

    Added PageMessage

---
+++ SocketBasedStorageServer/src/socketbasedstorageserver/PageMessage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketbasedstorageserver;
+
+/**
+ *
+ * @author ashwinbahulkar
+ */
+public class PageMessage {
+    
+    String message;
+    Page page;
+
+    public PageMessage(String message, Page page) {
+        this.message = message;
+        this.page = page;
+    }
+    
+    
+    
+}
